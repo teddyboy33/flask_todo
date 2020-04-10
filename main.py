2aa03651bb3af7a011bc54dcb6bd8d04609db4f1
Philip Korte <kortephilip@gmail.com>
2020-04-09 23:37:49 -0700

    I don't know

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from model import Task, User
 from loguru import logger
 
 app = Flask(__name__)
-# app.secret_key = b'\x9d\xb1u\x08%\xe0\xd0p\x9bEL\xf8JC\xa3\xf4J(hAh\xa4\xcdw\x12S*,u\xec\xb8\xb8'
-app.secret_key = os.environ.get('SECRET_KEY').encode()
+app.secret_key = b'\x9d\xb1u\x08%\xe0\xd0p\x9bEL\xf8JC\xa3\xf4J(hAh\xa4\xcdw\x12S*,u\xec\xb8\xb8'
+# app.secret_key = os.environ.get('SECRET_KEY').encode()
 
 @app.route('/all')
 def all_tasks():
